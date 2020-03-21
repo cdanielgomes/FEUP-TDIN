29ec872bf335b18c2d0a68f77a0b8108a5fc26fc
Raidenkyu <joao-carlos.14@hotmail.com>
2020-03-21 20:04:01 +0000

    Started GUI client

---
+++ Client/ClientWindow.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Runtime.Remoting;
+using System.Windows.Forms;
+using Common;
+
+namespace Client {
+    public partial class ClientWindow : Form {
+        private IServer ChatServer;
+        public ClientWindow() {
+            InitializeServerConnection();
+            InitializeComponent();
+            
+            if (ChatServer.RegisterUser("Random User")) {
+                Console.WriteLine("Registration worked");
+            }
+            else {
+                Console.WriteLine("Registration failed");
+            }
+
+        }
+
+        private void InitializeServerConnection() {
+            try {
+                RemotingConfiguration.Configure("Client.exe.config", false);
+                ChatServer = (IServer) RemoteNew.New(typeof(IServer));
+            }
+            catch (RemotingException e) {
+                Console.WriteLine("Failed to connect to Server:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Runtime.Remoting;
+using System.Windows.Forms;
 using Common;
 
 namespace Client {
     internal static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
         public static void Main(string[] args) {
-            RemotingConfiguration.Configure("Client.exe.config", false);
-            IServer server = (IServer) RemoteNew.New(typeof(IServer));
-            if (server.RegisterUser("Random User")) {
-                Console.WriteLine("Registration worked");
-            }
-            else {
-                Console.WriteLine("Registration failed");
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ClientWindow());
         }
     }
 }
