11384b4aadd28dfe17fcd44b3d7be4b4392527a0
Raidenkyu <joao-carlos.14@hotmail.com>
2020-05-11 15:59:19 +0100

    Added a GUI prototype

---
+++ Project 2/solver-app/MainWindow.cs
@@ -0,0 +1,24 @@
+using System;
+using Gtk;
+using UI = Gtk.Builder.ObjectAttribute;
+
+namespace solver_app
+{
+    class MainWindow : Window
+    {
+
+        public MainWindow() : this(new Builder("MainWindow.glade")) { }
+
+        private MainWindow(Builder builder) : base(builder.GetObject("MainWindow").Handle)
+        {
+            builder.Autoconnect(this);
+
+            DeleteEvent += Window_DeleteEvent;
+        }
+
+        private void Window_DeleteEvent(object sender, DeleteEventArgs a)
+        {
+            Application.Quit();
+        }
+    }
+}
