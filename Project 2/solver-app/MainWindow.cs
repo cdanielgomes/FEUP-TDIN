965dfde36cfa516dc41f1b508cdc891f99ddd0f9
Raidenkyu <joao-carlos.14@hotmail.com>
2020-05-12 12:42:01 +0100

    Improved GUI

--- Project 2/solver-app/MainWindow.cs
+++ Project 2/solver-app/MainWindow.cs
@@ -2,23 +2,31 @@ using System;
 using Gtk;
 using UI = Gtk.Builder.ObjectAttribute;
 
-namespace solver_app
-{
-    class MainWindow : Window
-    {
+namespace Solver {
+    class MainWindow : Window {
+
+        [UI]
+        ListBox myIssuesList = null;
 
         public MainWindow() : this(new Builder("MainWindow.glade")) { }
 
-        private MainWindow(Builder builder) : base(builder.GetObject("MainWindow").Handle)
-        {
+        private MainWindow(Builder builder) : base(builder.GetObject("MainWindow").Handle) {
             builder.Autoconnect(this);
 
             DeleteEvent += Window_DeleteEvent;
+
+            LoadIssuesList();
         }
 
-        private void Window_DeleteEvent(object sender, DeleteEventArgs a)
-        {
+        private void Window_DeleteEvent(object sender, DeleteEventArgs a) {
             Application.Quit();
+        }
+
+        private void LoadIssuesList() {
+            var listBoxRow = new ListBoxRow();
+            listBoxRow.Add(new Label { Text="Test Element", Expand=true});
+
+            myIssuesList.Insert(listBoxRow,0);
         }
     }
 }
