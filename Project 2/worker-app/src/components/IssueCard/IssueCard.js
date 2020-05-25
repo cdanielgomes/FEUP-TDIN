b95b33034d6004f4a56086107896bcd3076b5602
cdanielgomes <cdanielgomes@github.com>
2020-05-25 23:20:16 +0100

    resolution

--- Project 2/worker-app/src/components/IssueCard/IssueCard.js
+++ Project 2/worker-app/src/components/IssueCard/IssueCard.js
@@ -3,12 +3,12 @@ import { Card, Container, Row, Col } from "react-bootstrap"
 import moment from "moment"
 import styles from "./IssueCard.module.scss";
 
-const IssueCard = ({ title, createdAt, state, description }) => {
+const IssueCard = ({ title, createdAt, state, description, resolution }) => {
 
   const x = () => {
     return moment(createdAt).format('LLLL')
   }
-  
+
   return (
     <Card className={styles["issueCard"]}>
       <Card.Header as="h5" className={styles[`issue-${state}`]}>
@@ -20,7 +20,17 @@ const IssueCard = ({ title, createdAt, state, description }) => {
         </Container>
       </Card.Header>
       <Card.Body>
+        <Card.Title> {"Description"} </Card.Title>
         <Card.Text>{description}</Card.Text>
+        {
+          state === "solved" &&
+          <>
+          <br></br>
+            <Card.Title> {"Resolution"} </Card.Title>
+            <Card.Text>{resolution}</Card.Text>
+          </>
+        }
+
       </Card.Body>
       <Card.Footer> {x()} </Card.Footer>
     </Card>
